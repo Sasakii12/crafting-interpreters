9657706d9a193c6fac6139623e244c16ce584d7c
vivian <undertalefan812@gmail.com>
2026-07-23 14:10:22 -0400

    up to chapter 4.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::stdin;
+use std::env;
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+mod tokens;
+
+fn input() -> String {
+    let mut buf = String::new();
+
+    stdin().read_line(&mut buf).unwrap();
+
+    buf
+}
+
+
+
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("Usage: jlox [script]");
+        todo!()
+    } else if args.len() == 1 {
+        todo!()
+    } else {
+        todo!()
+    }
 }
---
+++ src/tokens.rs
@@ -0,0 +1,61 @@
+use core::fmt;
+
+#[derive(Debug, Clone, PartialEq)]
+enum TokenType {
+    // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Literal {
+    Str(String),
+    Bool(bool),
+    Number(f64),
+    Nil,
+}
+
+impl fmt::Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Literal::Str(s) => write!(f, "{}", s),
+            Literal::Number(n) => write!(f, "{}", n),
+            Literal::Bool(b) => write!(f, "{}", b),
+            Literal::Nil => write!(f, "nil"),
+        }
+    }
+}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Literal, line: usize) -> Self {
+       Token { token_type, lexeme, literal, line } 
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{:?} {} {}", self.token_type, self.lexeme, self.literal)
+    }
+}
